fe9c883d9d3bcab8e60dbb6a408ad1a2d2565e67
andrehahn05 <andrehahn05@gmail.com>
2022-07-12 13:50:34 -0400

    showService

--- src/modules/news/services/ShowNewsService.ts
+++ src/modules/news/services/ShowNewsService.ts
@@ -4,7 +4,6 @@ import NewsSchema from '@modules/news/infra/mongoose/schemas/NewsSchema';
 
 class ShowNewsService {
   public async execute(_id: string): Promise<INews | any> {
-    
     const news = await NewsSchema.findById(_id);
 
     if (!news) {
