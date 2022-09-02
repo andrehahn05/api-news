a10e054d25e593e05d594be2a1e54d3c4b4ed561
andrehahn05 <andrehahn05@gmail.com>
2022-09-01 20:14:42 -0400

    CreateUserService

--- src/modules/users/models/IUser.ts
+++ src/modules/users/models/IUser.ts
@@ -1,6 +1,6 @@
 export interface IUser {
   _id?:string | any;
-  name: string;
-  email: string;
-  password:string;
+  name?: string;
+  email?: string;
+  password?:string;
 }
---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,19 @@
+import { IUser } from '@modules/users/models/IUser';
+import AppError from 'src/shared/errors/AppError';
+import UserSchema from '@src/modules/users/infra/mongoose/schema/UserSchema';
+
+class CreateUserService {
+  public async execute({...data}: IUser):Promise<IUser>{
+    const existUser = await UserSchema.findOne({email: data.email});
+    
+    if(existUser){
+        throw new AppError('User is already registered.');
+    }
+
+    const user = await UserSchema.create({...data});
+    
+    return user;
+  }
+} 
+
+export default new CreateUserService;
