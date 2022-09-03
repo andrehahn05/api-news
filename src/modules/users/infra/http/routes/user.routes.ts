6b95ad85485c79e11c2231cdb3d0665872558e12
andrehahn05 <andrehahn05@gmail.com>
2022-09-03 19:16:29 -0400

    routes

--- src/modules/users/infra/http/routes/user.routes.ts
+++ src/modules/users/infra/http/routes/user.routes.ts
@@ -3,7 +3,7 @@ import { celebrate, Joi, Segments } from 'celebrate';
 import userController from '../controllers/UserController';
 
 const userRouter = Router();
-
+userRouter.get('/list', userController.index);
 userRouter.post(
   '/',
   celebrate({
@@ -16,4 +16,38 @@ userRouter.post(
   userController.store,
 );
 
+userRouter.get(
+  '/show/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  userController.show,
+);
+
+userRouter.delete(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+  }),
+  userController.delete,
+);
+
+userRouter.put(
+  '/:id',
+  celebrate({
+    [Segments.PARAMS]: {
+      id: Joi.string().required(),
+    },
+    [Segments.BODY]: {
+      name: Joi.string(),
+      email: Joi.string().email(),
+      password: Joi.string(),
+    },
+  }),
+  userController.update,
+);
 export default userRouter;
