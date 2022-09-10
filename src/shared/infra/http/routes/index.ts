8b368df64eac4b70dbf1f07df5fb1b3f94209696
andrehahn05 <andrehahn05@gmail.com>
2022-09-09 22:16:39 -0400

    route/session

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,9 +1,11 @@
-import { Router } from "express";
-import newsRouter from "@modules/news/infra/http/routes/news.routes";
-import userRouter from "@src/modules/users/infra/http/routes/user.routes";
+import { Router } from 'express';
+import newsRouter from '@modules/news/infra/http/routes/news.routes';
+import userRouter from '@src/modules/users/infra/http/routes/user.routes';
+import sessionsRoutes from '@src/modules/users/infra/http/routes/sessions.routes ';
 const routes = Router();
 
 routes.use('/api/v1/news',newsRouter)
 routes.use('/api/v1/user',userRouter)
+routes.use('/api/v1/session',sessionsRoutes)
 
 export default routes;
