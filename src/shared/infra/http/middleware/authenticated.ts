4fabcb49d337296fa68fa68d63381372959a44af
andrehahn05 <andrehahn05@gmail.com>
2022-09-09 22:11:18 -0400

    auth

---
+++ src/shared/infra/http/middleware/authenticated.ts
@@ -0,0 +1,39 @@
+import AppError from '@src/shared/errors/AppError';
+import { NextFunction, Request, Response } from 'express';
+import { verify, Secret, decode } from 'jsonwebtoken';
+import authConfig from '@src/config/auth';
+
+export interface ITokenPayload {
+  iat: number;
+  exp: number;
+  sub: string;
+}
+
+export function authenticated(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+): void {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader) {
+    console.log(authHeader)
+    throw new AppError('Token not provided.', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+  
+  try {
+    const decoded = verify(token, authConfig.jwt.secret as Secret);
+    
+    const { sub } = decoded as ITokenPayload;
+    req.user = {
+      id: sub,
+    };
+    
+    next();
+  } catch {
+    console.log(token);
+    throw new AppError(' Invalid Jwt token');
+  }
+}
